f431dde1a7db9c0d795779d34810844625fc5648
Kamen <sh.ibragim@gmail.com>
2022-03-05 01:32:08 +0300

    new folder

---
+++ test.js
@@ -0,0 +1,47 @@
+
+function minus(a,b){
+    let min = a-b;
+    alert(`a-b = ${min}`);
+}
+function plus(a,b){
+    let sum = a+b;
+    alert(`a+b = ${sum}`);
+} 
+
+function umn(a,b){
+    let um = a*b;
+    alert(`a*b = ${um}`);
+} 
+function del(a,b){
+    let d = a+b;
+    alert(`a/b = ${d}`);
+} 
+
+function pow(a,b){
+    let result = a;
+    for (let i=1; i<b; i++){
+        result *=a;
+    }
+return result;
+}
+let identinicator=promt('Выберите нужное действие: exp, -, +, /, *','');
+let a= prompt('a','');
+let b= prompt ('b','');
+if (identinicator="+"){
+    plus(a,b);
+}else if (identinicator="-"){
+    minus(a,b);
+}else if(identinicator="*"){
+    umn(a,b);
+}else if(identinicator="/"){
+    if(b=0){
+        alert("b=0, на ноль делить нельзя")}else{
+            del(a,b);
+        }
+}else if(identinicator="exp"){
+    if(b<1){
+     alert("Неверно задана степень")
+    }else{
+        pow(a,b);
+    }
+}
